995c8f8b1275c51e724559051bdc2505c8785ced
Iker Galardi <contacto.ikergalardi@gmail.com>
2023-01-31 21:05:49 +0100

    Add TCP server binding

--- c/src/main.c
+++ c/src/main.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 #include "common.h"
 #include "config.h"
@@ -7,6 +12,27 @@ int main(int argc, char** argv)
 {
     Config config = config_from_args(argc, argv);
 
-    printf("Hello world, port %d\n", config.port);
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if(fd == -1)
+    {
+        fprintf(stderr, "httpd: could not create socket: %s\n", strerror(errno));
+        goto close_socket;
+    }
+
+    struct sockaddr_in addr = {
+        .sin_family = AF_INET,
+        .sin_port = htons(config.port),
+        .sin_addr.s_addr = INADDR_ANY,
+    };
+    int errcode = bind(fd, (struct sockaddr*)&addr, sizeof(addr));
+    if(errcode == -1)
+    {
+        fprintf(stderr, "httpd: could not bind the socket to port %d: %s\n", config.port, strerror(errno));
+        goto close_socket;
+    }
+
+close_socket:
+    close(fd);
+
     return 0;
 }
