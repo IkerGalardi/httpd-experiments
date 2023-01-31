9022e7cd494781d1f1f609ece6c9928f55852d4f
Iker Galardi <contacto.ikergalardi@gmail.com>
2023-01-31 20:22:27 +0100

    Add simple argv parser for configuration

--- c/src/main.c
+++ c/src/main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 #include "common.h"
+#include "config.h"
 
 int main(int argc, char** argv)
 {
-    UNUSED_PARAM(argc);
-    UNUSED_PARAM(argv);
+    Config config = config_from_args(argc, argv);
 
-    printf("Hello world\n");
+    printf("Hello world, port %d\n", config.port);
     return 0;
 }
