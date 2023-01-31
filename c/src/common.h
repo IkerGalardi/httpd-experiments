c5bf488deec56a2b5c3d485cb33c177d277841ce
Iker Galardi <contacto.ikergalardi@gmail.com>
2023-01-31 19:55:32 +0100

    Project setup

---
+++ c/src/common.h
@@ -0,0 +1,6 @@
+#ifndef _COMMON_H
+#define _COMMON_H
+
+#define UNUSED_PARAM(x) (void)(x)
+
+#endif // _COMMON_H
---
+++ c/src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#include "common.h"
+
+int main(int argc, char** argv)
+{
+    UNUSED_PARAM(argc);
+    UNUSED_PARAM(argv);
+
+    printf("Hello world\n");
+    return 0;
+}
